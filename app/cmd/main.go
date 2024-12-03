2fe15af3097840ad1d2d11daf058b73cb6697a51
YutaKakiki <yutakakiki@kakinokiyuunoMacBook-Air.local>
2024-12-03 15:27:32 +0900

    dbコンテナのセットアップ

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -1,1 +1,10 @@
 package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		println("Hello,world!")
+	})
+	http.ListenAndServe(":8080", nil)
+}
