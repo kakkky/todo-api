1bdce79bc761a2428049bb9c73821d2104132257
YutaKakiki <yutakakiki@kakinokiyuunoMacBook-Air.local>
2024-12-18 14:27:11 +0900

    ロガーミドルウェアを実装

--- app/infrastructure/router/mux_user.go
+++ app/infrastructure/router/mux_user.go
@@ -3,6 +3,7 @@ package router
 import (
 	"net/http"
 
+	"github.com/kakkky/app/adapter/presentation/middleware"
 	userHandler "github.com/kakkky/app/adapter/presentation/user"
 	"github.com/kakkky/app/adapter/repository"
 	userUsecase "github.com/kakkky/app/application/usecase/user"
@@ -12,16 +13,24 @@ import (
 func handleUser(mux *http.ServeMux) {
 	userRepository := repository.NewUserRepository()
 
-	mux.Handle("POST /user", userHandler.NewPostUserHandler(userUsecase.NewRegisterUsecase(
-		userRepository, user.NewUserDomainService(userRepository),
-	)))
-	mux.Handle("DELETE /user/{id}", userHandler.NewDeleteUserHandler(userUsecase.NewUnregisterUsecase(
-		userRepository,
-	)))
-	mux.Handle("GET /users", userHandler.NewGetUsersHandler(userUsecase.NewListUsersUsecase(
-		userRepository,
-	)))
-	mux.Handle("PATCH /user", userHandler.NewUpdateUserHandler(userUsecase.NewUpdateProfileUsecase(
-		userRepository,
-	)))
+	mux.Handle("POST /user", composeMiddlewares(middleware.Logger)(
+		userHandler.NewPostUserHandler(
+			userUsecase.NewRegisterUsecase(
+				userRepository,
+				user.NewUserDomainService(userRepository),
+			))))
+	mux.Handle("DELETE /user/{id}", composeMiddlewares(middleware.Logger)(
+		userHandler.NewDeleteUserHandler(
+			userUsecase.NewUnregisterUsecase(
+				userRepository,
+			))))
+	mux.Handle("GET /users", composeMiddlewares(middleware.Logger)(
+		userHandler.NewGetUsersHandler(
+			userUsecase.NewListUsersUsecase(
+				userRepository,
+			))))
+	mux.Handle("PATCH /user", composeMiddlewares(middleware.Logger)(
+		userHandler.NewUpdateUserHandler(userUsecase.NewUpdateProfileUsecase(
+			userRepository,
+		))))
 }
